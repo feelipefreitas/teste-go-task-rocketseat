6dee893f829bf511c2b617d367d84cf0c7bdad7f
feelipefreitas <feelipe.freitas97@gmail.com>
2025-04-21 14:30:24 -0300

    Criação parcial da funcionalidade de drag and drop com subscribe no componente

--- src/app/components/task-list-section/task-list-section.component.ts
+++ src/app/components/task-list-section/task-list-section.component.ts
@@ -1,19 +1,53 @@
 import { Component, inject } from '@angular/core';
 import { TaskCardComponent } from '../task-card/task-card.component';
-import { TaskService } from '../../services/task.service';
+import { ITask, TaskService } from '../../services/task.service';
+import { AsyncPipe } from '@angular/common';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-task-list-section',
-  imports: [TaskCardComponent],
+  imports: [TaskCardComponent, AsyncPipe, CdkDropList, CdkDrag],
   templateUrl: './task-list-section.component.html',
   styleUrl: './task-list-section.component.css'
 })
 export class TaskListSectionComponent {
-  private readonly _taskService = inject(TaskService);
+  toDoTasksDropList: ITask[] = [];
+  doingTasksDropList: ITask[] = [];
+  doneTasksDropList: ITask[] = [];
+
+  readonly _taskService = inject(TaskService);
 
   ngOnInit() {
     this._taskService.toDoTasks.subscribe((toDoTasks) => {
-      console.log('Lista atual de tarefas: ', toDoTasks);
+      console.log('Lista atual de tarefas TOdO: ', toDoTasks);
+      this.toDoTasksDropList = toDoTasks;
     });
+    this._taskService.doingTasks.subscribe((doingTasks) => {
+      console.log('Lista atual de tarefas doing: ', doingTasks);
+      this.doingTasksDropList = doingTasks;
+    });
+    this._taskService.doneTasks.subscribe((doneTasks) => {
+      console.log('Lista atual de tarefas done: ', doneTasks);
+      this.doneTasksDropList = doneTasks;
+    });
+  }
+
+  drop(event: CdkDragDrop<ITask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 }
