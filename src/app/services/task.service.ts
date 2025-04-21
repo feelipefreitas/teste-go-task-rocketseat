a21e75ac6567be6f8f5a84b267b54f33a43d69b5
feelipefreitas <feelipe.freitas97@gmail.com>
2025-04-21 13:33:12 -0300

    Consumo da lista de todos

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map } from 'rxjs';
+import { ITaskFormControls } from '../components/task-form-modal/task-form-modal.component';
 
 export interface ITask {
   id: string;
-  title: string;
+  name: string;
   description: string;
   comments: ITaskComment[];
   status: 'to-do' | 'doing' | 'done';
@@ -12,7 +13,7 @@ export interface ITask {
 export interface ITaskComment {
   id: string;
   description: string;
-  dayOfComment: string;
+  dayOfComment: string; //TODO: qual é a melhor forma de salver essa data
 }
 
 @Injectable({
@@ -21,17 +22,36 @@ export interface ITaskComment {
 export class TaskService {
   // Tarefas em A Fazer
   private toDoTasks$ = new BehaviorSubject<ITask[]>([]);
-  readonly toDoTasks = this.toDoTasks$.asObservable();
+  readonly toDoTasks = this.toDoTasks$.asObservable().pipe(
+    map(tasks => tasks.map(task => ({ ...task, comments: [...task.comments] })))
+  );
 
   // Tarefas em Fazendo
   private doingTasks$ = new BehaviorSubject<ITask[]>([]);
-  readonly doingTasks = this.doingTasks$.asObservable();
+  readonly doingTasks = this.doingTasks$.asObservable().pipe(
+    map(tasks => tasks.map(task => ({ ...task, comments: [...task.comments] })))
+  );
 
   // Tarefas em Concluído
   private doneTasks$ = new BehaviorSubject<ITask[]>([]);
-  readonly doneTasks = this.doneTasks$.asObservable();
+  readonly doneTasks = this.doneTasks$.asObservable().pipe(
+    map(tasks => tasks.map(task => ({ ...task, comments: [...task.comments] })))
+  );
 
-  generateUniqueIdWithTimestamp(): string {
+  addTask(taskInfos: ITaskFormControls) {
+    const newTask: ITask = {
+      ...taskInfos,
+      status: 'to-do',
+      id: this.generateUniqueIdWithTimestamp(),
+      comments: [],
+    };
+
+    const currentList = this.toDoTasks$.value;
+
+    this.toDoTasks$.next([...currentList, newTask]);
+  }
+
+  private generateUniqueIdWithTimestamp(): string {
     const timestamp = new Date().getTime().toString(36); // Convert current timestamp to base 36
     const randomPart = Math.random().toString(36).substring(2, 9); // Shorter random part
     return `${timestamp}-${randomPart}`;
